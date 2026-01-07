e39ac9757459540f7ac32192eb903b2054649a40
coderhisham <imhish4m@gmail.com>
2026-01-08 04:31:03 +0530

    boilerplate setup

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@web/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button className="mt-4 px-4 py-2 text-white rounded">Hello</Button>
+    </>
+  );
+}
